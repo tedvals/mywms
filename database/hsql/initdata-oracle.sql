cd6589e2924c9c145b7255e936cfd3f5d0ec677e
Andreas Pasiopoulos <pasiopou@gmail.com>
2012-05-16 19:54:37 +0300

    jboss starts with some queue errors

--- database/hsql/initdata-oracle.sql
+++ database/hsql/initdata-oracle.sql
@@ -1,44 +1,12 @@
-CREATE OR REPLACE function jboss.md5hash(in_string in varchar2)
-return varchar2 as cln_md5raw raw(2000); out_raw raw(16); begin cln_md5raw := utl_raw.cast_to_raw(in_string);dbms_obfuscation_toolkit.md5(input=>cln_md5raw,checksum=>out_raw);return lower(rawtohex(out_raw));end;;
-
-insert into jboss.mywms_client(id, version, entity_lock, created, modified, 
-name, CL_NR, cl_code, 
-additionalcontent) 
-values(0, 0, 0, current_timestamp, current_timestamp, 
-'System-Client', 'System', 'System-Client',
-'This is a system used entity. DO NOT REMOVE OR LOCK IT! Some processes may use it. But feel free to choose a suitable name.');
-
-insert into jboss.mywms_role(id, version, entity_lock, created, modified,
-name, description,
-additionalcontent)
-values(0, 0, 0, current_timestamp, current_timestamp, 
-'Admin', 'System Administrator',
-'This is a system used entity. DO NOT REMOVE, LOCK OR RENAME IT! Some processes may use it.');
-
-insert into jboss.mywms_user(id, version, entity_lock, created, modified, 
-name, locale, password, client_id, 
-additionalcontent) 
-values(0, 0, 0, current_timestamp, current_timestamp, 
-'admin', 'en', jboss.md5hash('admin'), 0,
-'This is a system used entity. DO NOT REMOVE OR LOCK IT! Some processes may use it. But feel free to choose a suitable name and password.');
-
-insert into jboss.mywms_user(id, version, entity_lock, created, modified, 
-name, locale, password, client_id) 
-values(1, 0, 0, current_timestamp, current_timestamp, 
-'deutsch', 'de', jboss.md5hash('deutsch'), 0);
-
-insert into jboss.mywms_user(id, version, entity_lock, created, modified, 
-name, locale, password, client_id) 
-values(2, 0, 0, current_timestamp, current_timestamp, 
-'english', 'en', jboss.md5hash('english'), 0);
-
-insert into jboss.mywms_user(id, version, entity_lock, created, modified, 
-name, locale, password, client_id) 
-values(4, 0, 0, current_timestamp, current_timestamp, 
-'francais', 'fr', jboss.md5hash('francais'), 0);
-
-insert into jboss.mywms_user_jboss.mywms_role(jboss.mywms_user_id, roles_id)
-(select u.id, r.id 
-from jboss.mywms_user u , jboss.mywms_role r
-where u.name in ('admin','deutsch','english','francais') and
-r.name='Admin');
+CREATE ALIAS MD5 FOR "org.hsqldb.lib.MD5.encodeString";
+INSERT INTO MYWMS_CLIENT VALUES(0,'2010-11-30 13:59:22.553000000','2010-11-30 14:32:03.232000000','This is a system used entity. DO NOT REMOVE, LOCK OR RENAME IT! Some processes may use it.',0,1,'01',NULL,NULL,'System-Client','System',NULL);
+INSERT INTO MYWMS_ROLE VALUES(0,'2010-11-30 13:59:51.888000000','2010-11-30 13:59:51.888000000','This is a system used entity. DO NOT REMOVE, LOCK OR RENAME IT! Some processes may use it.',0,0,'Admin','System Administrator');
+INSERT INTO MYWMS_ROLE VALUES(600,'2010-11-30 14:32:03.228000000','2010-11-30 14:32:03.229000000',NULL,0,0,'Operator','');
+INSERT INTO MYWMS_ROLE VALUES(601,'2010-11-30 14:32:03.242000000','2010-11-30 14:32:03.246000000',NULL,0,0,'Foreman','');
+INSERT INTO MYWMS_ROLE VALUES(602,'2010-11-30 14:32:03.248000000','2010-11-30 14:32:03.248000000',NULL,0,0,'Inventory','');
+INSERT INTO MYWMS_USER VALUES(0,'2010-11-30 13:59:51.916000000','2010-11-30 13:59:51.916000000','This is a system used entity. DO NOT REMOVE OR LOCK IT! Some processes may use it. But feel free to choose a suitable name and password.',0,0,NULL,NULL,NULL,NULL,'admin','en','21232f297a57a5a743894a0e4a801fc3',0);
+INSERT INTO MYWMS_USER VALUES(1,'2010-11-30 13:59:51.922000000','2010-11-30 14:32:03.267000000',0,1,NULL,NULL,NULL,NULL,'Deutsch','deutsch','de','09c438e63455e3e1b3deabe65fdbc087',0);
+INSERT INTO MYWMS_USER VALUES(2,'2010-11-30 13:59:51.925000000','2010-11-30 14:32:03.275000000',0,1,NULL,NULL,NULL,NULL,'English','english','en','ba0a6ddd94c73698a3658f92ac222f8a',0);
+INSERT INTO MYWMS_USER_MYWMS_ROLE VALUES(0,0);
+INSERT INTO MYWMS_USER_MYWMS_ROLE VALUES(1,0);
+INSERT INTO MYWMS_USER_MYWMS_ROLE VALUES(2,0);
